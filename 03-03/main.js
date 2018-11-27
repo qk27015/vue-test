98e9016e36d7d3699e9a0aa81d39fcff2909fafc
qk27015 <tk5760a@gmail.com>
2018-11-27 21:35:46 +0900

    +

--- 03-03/main.js
+++ 03-03/main.js
@@ -5,7 +5,6 @@ Vue.component('item-desc', {
     // キャメルケースで記述
     itemTest: {type: String}
   },
-
   // 受け取った属性をtemplateで使用可能
   template: '<p>{{itemTest}}は便利です。</p>'
 });
@@ -25,14 +24,38 @@ Vue.component('fruits-item-name', {
   },
   template: '<li>{{fruitsItem.name}}</li>'
 });
-
 new Vue({
     el: '#fruits-component',
     data: {
       fruitsItems: [
         {name: '梨'},
         {name: 'イチゴ'}
-      ],
-      f: 'test'
+      ]
     }
+});
+
+
+
+// 子→親
+var counterButton = Vue.extend({
+  template: '<span>{{counter}}個<button v-on:click="addToCart">追加</button></span>',
+  data : function(){ return {counter: 0};},
+  methods: {addToCart: function(){
+    this.counter += 1;
+    this.$emit('increment'); // カスタムイベントの発火
+  }}
+});
+new Vue({
+  el: '#fruits-counter',
+  components: {'counter-button': counterButton},
+  data: {
+    total: 0,
+    fruits: [
+      {name: '梨'},
+      {name: 'イチゴ'}
+    ]
+  },
+  methods: {
+    incrementCartStatus: function(){this.total += 1;}
+  }
 });
